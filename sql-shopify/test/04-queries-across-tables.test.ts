9b0da9de977026c5b01c9289a4ef18c26ab6638b
Girts Varna <gvarna@gmail.com>
2022-02-01 12:51:44 +0200

    test 04-queries-across-tables.test completed

--- sql-shopify/test/04-queries-across-tables.test.ts
+++ sql-shopify/test/04-queries-across-tables.test.ts
@@ -9,7 +9,11 @@ describe("Queries Across Tables", () => {
     }, minutes(1));
 
     it("should select count of apps which have free pricing plan", async done => {
-        const query = `todo`;
+        const query = `
+        SELECT COUNT (*) AS count FROM apps
+        JOIN apps_pricing_plans ON apps.id = apps_pricing_plans.app_id
+        JOIN pricing_plans ON apps_pricing_plans.pricing_plan_id = pricing_plans.id
+        WHERE pricing_plans.price LIKE 'Free%'`;
         const result = await db.selectSingleRow(query);
         expect(result).toEqual({
             count: 1112
@@ -18,7 +22,13 @@ describe("Queries Across Tables", () => {
     }, minutes(1));
 
     it("should select top 3 most common categories", async done => {
-        const query = `todo`;
+        const query = `
+        SELECT COUNT (*) AS count, categories.title AS category FROM apps
+        JOIN apps_categories ON apps.id = apps_categories.app_id
+        JOIN categories ON apps_categories.category_id = categories.id
+        GROUP BY category
+        ORDER BY count DESC
+        LIMIT 3`;
         const result = await db.selectMultipleRows(query);
         expect(result).toEqual([
             { count: 1193, category: "Store design" },
@@ -29,7 +39,21 @@ describe("Queries Across Tables", () => {
     }, minutes(1));
 
     it("should select top 3 prices by appearance in apps and in price range from $5 to $10 inclusive (not matters monthly or one time payment)", async done => {
-        const query = `todo`;
+        const query = `
+        SELECT
+            COUNT (*) AS count, price, 
+            CASE
+                WHEN price LIKE '%/%' THEN CAST(substr(price, 2, instr(price,'/') -2) AS REAL)
+                WHEN price LIKE '%one%' THEN CAST(substr(price, 2, instr(price,'o') -3) AS REAL)
+                ELSE CAST('0' AS REAL)
+            END casted_price
+        FROM apps
+        JOIN apps_pricing_plans ON apps.id = apps_pricing_plans.app_id
+        JOIN pricing_plans ON apps_pricing_plans.pricing_plan_id = pricing_plans.id
+        WHERE casted_price BETWEEN 5 AND 10
+        GROUP BY casted_price
+        ORDER BY count DESC
+        LIMIT 3`;
         const result = await db.selectMultipleRows(query);
         expect(result).toEqual([
             { count: 225, price: "$9.99/month", casted_price: 9.99 },
